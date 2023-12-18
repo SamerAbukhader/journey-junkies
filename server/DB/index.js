97b6c4e5606d0f303989fe8fe11ce2d7f951d82f
SamerAbukhader <sakhader_2003@hotmail.com>
2023-12-19 01:32:25 +0800

    Connected to a GLOBAL DB by me

--- server/DB/index.js
+++ server/DB/index.js
@@ -2,9 +2,9 @@ import mysql from "mysql";
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "202.186.113.246",
+  user: "JJDB",
+  password: "HOp5PaViSef3q1f27IzaPoKe2an4nA",
   database: "journy_junkies",
 });
 export default pool;
