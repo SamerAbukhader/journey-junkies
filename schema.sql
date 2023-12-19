b8de07ae1fef4974033e157c59a0737b0505e7c4
SamerAbukhader <sakhader_2003@hotmail.com>
2023-12-19 18:48:48 +0800

    Deleted Unused Code + Update Schema

--- schema.sql
+++ schema.sql
@@ -1,42 +1,73 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- Create the User table
-CREATE TABLE "User" (
-  id VARCHAR(255) PRIMARY KEY,
-  email VARCHAR(255),
-  firstname VARCHAR(255),
-  lastname VARCHAR(255),
-  nationality VARCHAR(255),
-  activedate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  joindate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  birthdate DATE, 
-  image TEXT,
-  isadmin BOOLEAN NOT NULL DEFAULT FALSE,
-  viewcount INTEGER,
-  verified BOOLEAN NOT NULL DEFAULT FALSE
-);
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.5.0.6677
+-- --------------------------------------------------------
 
--- Create the Post table
-CREATE TABLE Post (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  author VARCHAR(255) REFERENCES "User"(id),
-  title VARCHAR(255),
-  content TEXT,
-  description TEXT,
-  image TEXT,
-  rating FLOAT
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Create the Rating table
-CREATE TABLE Rating (
-  post uuid REFERENCES Post(id),
-  author VARCHAR(255) REFERENCES "User"(id),
-  rate FLOAT
-);
 
--- Create the Comments table
-CREATE TABLE Comments (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  post uuid REFERENCES Post(id),
-  author VARCHAR(255) REFERENCES "User"(id),
-  comment TEXT
-);
+-- Dumping database structure for journy_junkies
+CREATE DATABASE IF NOT EXISTS `journy_junkies` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `journy_junkies`;
+
+-- Dumping structure for table journy_junkies.comments
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `author` text NOT NULL,
+  `comment` text NOT NULL,
+  `post_id` int(11) NOT NULL DEFAULT 0,
+  `author_id` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__posts` (`post_id`),
+  CONSTRAINT `FK__posts` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table journy_junkies.posts
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `author` text DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `image` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `location` text DEFAULT NULL,
+  `tag` text DEFAULT NULL,
+  `map_coords` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table journy_junkies.ratings
+CREATE TABLE IF NOT EXISTS `ratings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `user` text DEFAULT NULL,
+  `rating` float DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `post_author` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ratings_posts` (`post_id`),
+  CONSTRAINT `FK_ratings_posts` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
