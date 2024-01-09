6a2eb8c232f4472ec821bf8291b787b6998a21dd
SamerAbukhader <sakhader_2003@hotmail.com>
2024-01-09 17:45:52 +0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: "journyjunkies.ddns.net",
     port: 3000,
     proxy: {
       "/api": {
